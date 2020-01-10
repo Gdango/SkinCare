b8695f6011d60cf9494436886c3c582d6c7752a2
Gdango <vthhuynh@ucdavis.edu>
2020-01-09 18:05:31 -0800

    loop through pages and skin type

--- Sephora_WebScraping.py
+++ Sephora_WebScraping.py
@@ -1,23 +1,31 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 
-url = "https://www.ulta.com/skin-care-moisturizers?N=2796"
-
-url_dry = "https://www.ulta.com/skin-care-moisturizers?N=2796Z1z13p3o" #6 pages
-url_normal = "https://www.ulta.com/skin-care-moisturizers?N=2796Z1z13p3l" #6 pages
-url_combination = "https://www.ulta.com/skin-care-moisturizers?N=2796Z1z13p3l" #6 pages
-url_oily = "https://www.ulta.com/skin-care-moisturizers?N=2796Z1z13p3j" #5 pages
-url_sensitive = "https://www.ulta.com/skin-care-moisturizers?N=2796Z1z13p3m" #4 pages
+url_base = "https://www.ulta.com/skin-care-moisturizers?N=2796"
 
 
+url_dry = "Z1z13p3o" #6 pages
+url_normal = "Z1z13p3l" #6 pages
+url_combination = "Z1z13p3l" #6 pages
+url_oily = "Z1z13p3j" #5 pages
+url_sensitive = "Z1z13p3m" #4 pages
 
 
 #filename_concern = ["dryness", "anti_aging", "dark_spots", "tone", "redness", "oiliness", "acne", "blackhead", "finelines", "darkcircles"]
 filename_skintype = ["dry", "normal", "combination", "oily", "sensitive"]
-urls = [url_dry, url_normal, url_combination, url_oily, url_sensitive]
+url_parts = [url_dry, url_normal, url_combination, url_oily, url_sensitive]
 
 #for each url, need access to every page
 url = url_dry + '&No=' + str(96*2) + '&Nrpp=96'
+pages = 6
+
+for i in range(0, len(url_parts)):
+    url = url_base + url_part[i]
+    info(filename_skintype[i], url)
+    for page in range(1, pages):
+        url = url_base + url_part[i] + '&No=' + str(96*page) + '&Nrpp=96'
+        info(filename_skintype[i] + str(page+1), url)
+
 
 def info(file_name, url):
     uClient = uReq(url)
