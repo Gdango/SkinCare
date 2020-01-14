fe7eb2417d33da14ba3fda980af209a523342223
Gdango <vthhuynh@ucdavis.edu>
2020-01-13 22:52:34 -0800

    import data

--- analysis/dry_analysis.sql
+++ analysis/dry_analysis.sql
@@ -8,5 +8,9 @@ CREATE TABLE Dry
   price character varying(50)                                     
   --max_amount character varying(50)
  );                 
+
+-- imported table from command line using \copy:
+--psql -h <host> -U <username> -d <dbname> -c "\copy Dry FROM 'path to file' with delimiter ',';"
+
  
        
