801549c36137836c7de9b4a0999093182d84925e
Gdango <vthhuynh@ucdavis.edu>
2020-02-13 21:39:22 -0800

    graphing average cost of each brand

--- analysis/Brand_Under_Fifty.py
+++ analysis/Brand_Under_Fifty.py
@@ -2,7 +2,7 @@ import get_info
 import matplotlib.pyplot as plt
 import numpy as np
 
-query = 'select brand, avg(max_amount) from combination group by brand order by avg(max_amount) desc;'
+query = 'select brand, avg(max_amount) from combination where rating >= 4 group by brand order by avg(max_amount) desc;'
 
 info = get_info.get_info(query)
 
@@ -12,38 +12,28 @@ brand = info[1]
 def graph_info(cost, brand, ind, i):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.bar(np.arange(1, i+1), cost[ind:i])
-    plt.xticks(np.arange(1, i+1, 1))
+    ax.bar(np.arange(ind+1, i+1), cost[ind:i])
+    plt.xticks(np.arange(ind+1, i+1, 1), rotation = 'vertical')
     ax.set_xticklabels(brand[ind:i])
+    ax.set_xlabel('Brand Name')
+    ax.set_ylabel('Average Cost')
+    ax.set_title('Average Cost for Brand Greater Than 4 Stars')
     plt.show()
 
-cost100 = 0
+cost60 = 0
 cost50 = 0
 cost0 = 0
 
 for i in range(0,len(cost)):
-    print("YE")
-    if cost[i] >= 100:
-        
-        if cost100 > cost[i]:
-        cost100 = i
-        graph_info(cost, brand, 0, cost100)
-    elif cost[i] >= 50 and cost100 > i:
+    if cost[i] >= 60:  # if cost is greater than 100 dollars
+        cost60 = i
+    elif cost[i] >= 30:
         cost50 = i
-        graph_info(cost, brand, cost100, cost50)
-    elif cost[i] >= 0 and cost0 > i:
-        print("Yes")
+    elif cost[i] >= 0:
         cost0 = i
-        graph_info(cost, brand, cost50, cost0)
+# 0 20 75
 
+graph_info(cost, brand, 0, cost60)
+graph_info(cost, brand, cost60, cost50)
+graph_info(cost, brand, cost50, cost0)
 
-
-
-'''
-#plt.subplot(212)
-ax0.bar(np.arange(1,top+1), info[0][top:top*2])
-plt.xticks(np.arange(1, top+1, 1))
-ax0.set_xticklabels(info[1][top:top*2])
-
-ax1.set_xlabel('Brand Name')
-ax1.set_ylabel('Average Cost')'''
