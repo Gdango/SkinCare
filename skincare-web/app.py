69c835c3f73c5e5627cdbca518f075d36c44c629
Gdango <vthhuynh@ucdavis.edu>
2020-03-08 22:46:55 -0700

    flask takes and read user inputs

--- skincare-web/app.py
+++ skincare-web/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 
 def index():
+    if request.method == "POST":
+        skin_type = request.form.get("skin_type")
+        rating = request.form.get("Rating")
+        price = request.form.get("Price")
+
+        print(skin_type, rating, price)
     return render_template('index.html')
+
 
 
 if __name__ == "__main__":
