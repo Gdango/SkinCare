eb185c25c2c949957ce0522f3396e93495f347a7
Gdango <vthhuynh@ucdavis.edu>
2020-06-21 02:05:47 -0700

    unit test on response and refactored def Query

--- skincare-web/test_app.py
+++ skincare-web/test_app.py
@@ -2,7 +2,6 @@ from app import app
 import unittest
 
 class FlaskApp(unittest.TestCase):
-
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
@@ -16,4 +15,3 @@ if __name__ == "__main__":
 
     
 
-    
