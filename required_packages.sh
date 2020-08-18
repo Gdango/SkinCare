48c211044cde9e29c981e1a89dd66209ad296a13
Gdango <vthhuynh@ucdavis.edu>
2020-08-17 21:47:54 -0700

    credit

--- required_packages.sh
+++ required_packages.sh
@@ -4,3 +4,6 @@ sudo apt-get install python3-matplotlib
 #required to connect to aws database
 sudo apt-get update
 sudo apt-get install python3-psycopg2
+
+#install unit testing for flask
+pip3 install Flask-Testing
