3f60ff7316319cdc0f14e9d7684d06afc8408c63
Gdango <vthhuynh@ucdavis.edu>
2020-10-14 23:17:14 -0700

    reorganize folders

--- required_packages.sh
+++
@@ -1,9 +0,0 @@
-#install matplotlib from python
-sudo apt-get install python3-matplotlib
-
-#required to connect to aws database
-sudo apt-get update
-sudo apt-get install python3-psycopg2
-
-#install unit testing for flask
-pip3 install Flask-Testing
