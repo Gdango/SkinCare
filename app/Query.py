d26d057780e63f0acad61c3fe6a6b6ba29514393
Gdango <vthhuynh@ucdavis.edu>
2021-09-06 21:37:52 -0700

    add queries

--- app/Query.py
+++ app/Query.py
@@ -1,36 +1,48 @@
-class Query:
-    def __init__(self, skin_type, rating, price, order_by):
-        self.skin_type = skin_type
-        self.rating = rating
-        self.price = price
-        self.order_by = order_by
+class Query(self):
 
-    def _desired_price(self):
-        if self.price == '25-to-70':
-            query = f'''select * from {self.skin_type} 
-                    where (rating >= {self.rating}) 
-                    and (max_amount between {self.price.split('-to-', 1)[0]} 
-                    and {self.price.split('-to-',1)[1]})
-                    order by {self.order_by};'''
-        elif self.price == '70':
-            query = f'''select * from {self.skin_type}
-                    where (rating >= {self.rating})
+    def _desired_price(self, skin_type, price, rating, order_by):
+        if price == '25-to-70':
+            query = f'''select * from {skin_type} 
+                    where (rating >= {rating}) 
+                    and (max_amount between {price.split('-to-', 1)[0]} 
+                    and {price.split('-to-',1)[1]})
+                    order by {order_by};'''
+        elif price == '70':
+            query = f'''select * from {skin_type}
+                    where (rating >= {rating})
                     and max_amount >= 70
-                    order by {self.order_by};'''
+                    order by {order_by};'''
 
-        elif self.price == '25':
-            query = f'''select * from {self.skin_type}
-                    where (rating >= {self.rating})
+        elif price == '25':
+            query = f'''select * from {skin_type}
+                    where (rating >= {rating})
                     and max_amount <= 25
-                    order by {self.order_by};'''
+                    order by {order_by};'''
         return query
 
     def create_table(self, table_name):
         query = f'''create table {table_name} (
-            product_id INTEGER(4294967295)
+            line_id serial primary key,
+            product_id INT,
             brand VARCHAR (200),
             prod_name VARCHAR (200),
             rating FOAT(2),
+            min_amount float,
+            max_amount float, 
+            product_type VARCHAR (200),
+            skin_type VARCHAR (200)
         )'''
+        return query
+
+    def update_table(self, table_name, data):
+        query = f'''update {table_name}
+        set product_id = data[0],
+        brand = data[1],
+        prod_name = data[2],
+        rating = data[3],
+        min_amount = data[4],
+        max_amount = data[5],
+        product_type = data[6],
+        skin_type = data[7]'''
 
         return query
