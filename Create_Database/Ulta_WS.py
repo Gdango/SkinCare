dd642cb415dcc5364238583af95daeb5153725cc
Gdango <vthhuynh@ucdavis.edu>
2021-09-15 23:10:28 -0700

    add data to excel file

--- Create_Database/Ulta_WS.py
+++ Create_Database/Ulta_WS.py
@@ -5,30 +5,25 @@ import threading
 
 class Ulta:
   
-    def __init__(self, url):
+    def __init__(self, url, new_url=None):
         self.url = url
+        self.new_url = new_url
         #self.file = None
 
     #filename_concern = ["dryness", "anti_aging", "dark_spots", "tone", "redness", "oiliness", "acne", "blackhead", "finelines", "darkcircles"]filename_skintype = ["dry_skin.csv", "normal.csv", "combination.csv", "oily.csv", "sensitive.csv"]
     def _soup_page(self):
-        uClient = uReq(self.url)
+        uClient = uReq(self.new_url)
         page_html = uClient.read()
         uClient.close()
         return soup(page_html, "html.parser")
     
     #looping through the pages
-    def _pages(self, url_bases, url_base, url_parts, url_part, constant):
-        brand = []
-        prod_name = []
-        rating = []
-        price = []
-        prod_id = []
-        link = []
-        page = 1
-        while True:
+    def pages(self, product_type, skin_type, constant):
+        page = 0
+        while page <= 1:
             try:
-                url = f"{url}'&No='{str(constant*page)}'&Nrpp=96'"
-                self.info()
+                self.new_url = f'''{self.url}&No={constant*page}&Nrpp=96'''
+                self.info(product_type, skin_type)
                 page += 1
             except:
                 break
@@ -79,20 +74,29 @@ class Ulta:
             
             #write the data set into the cvs file
 
-    def info(self, brand, prod_name, rating, price, prod_id, link):
+    def info(self, product_type, skin_type):
+        f = open(skin_type, "w")
         for container in self.containers():
+            brand = self._info_brand(container)
+            prod_name = self._info_prod_name(container)
+            rating = self._info_rating(container)
+            price = self._info_price(container)
+            prod_id = self._product_id(container)
+            link = self._prod_link(container)
+
+            #not going to store it in a variable.  Took too long to run
+            ''' 
             brand.append(self._info_brand(container))
             prod_name.append(self._info_prod_name(container))
             rating.append(self._info_rating(container))
             price.append(self._info_price(container))
             prod_id.append(self._product_id(container))
-            link.append(self._prod_link(container))
+            link.append(self._prod_link(container))'''
 
-        return brand, prod_name, rating, price, prod_id, link
-
-            #self.file.write(product_id + "," + brand + "," + prod_name + "," + rating + "," + price + "," + link+"\n")
-
-
+        #return brand, prod_name, rating, price, prod_id, link 
+            #file.write(f'''{product_id},{brand},{prod_name},{rating},{price},{link},{product_type},{skin_type}\n''')
+            f.write(product_id + "," + brand + "," + prod_name + "," + rating + "," + price + "," + link+"\n")
+        f.close()
 
 
 
--- Create_Database/WebScraping.py
+++ Create_Database/WebScraping.py
@@ -7,14 +7,13 @@ def Ulta_info():
                 "cleanser": "https://www.ulta.com/skin-care-cleansers?N=2794"}
     url_parts = {"url_dry": "Z1z13p3o", "url_normal": "Z1z13p3l", "url_combination": "Z1z13p3k", "url_oily": "Z1z13p3j", "url_sensitive": "Z1z13p3m"}
     constant = 96
+    skin_type = "Dry"
+    product_type = "moisturizer"
 
     url = f"{url_bases['moisturizer']}{url_parts['url_dry']}"
 
     # returned data in order of brand, prod_name, rating, price, prod_id, link
     P = Ulta(url)
-    data = P.info()
+    P.pages(product_type, skin_type, constant)
 
-    return data
-
-
-print(Ulta_info())
+Ulta_info()
